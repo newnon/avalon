ee8298a412185d3d0b7415875d42e461b3083a05
Sergey <non_2001@mail.ru>
2016-05-20 14:10:48 +0300

    fix vk payments

--- avalon/platform/emcc/payment/payment.cpp
+++ avalon/platform/emcc/payment/payment.cpp
@@ -84,7 +84,16 @@ public:
                 {
                     // printf("parse  json item %s == %s \n", product.productIdentifier.c_str(), item["ID"].GetString());
 
-                    if (product.productIdentifier == std::string(std::string("pokerist_test.") + std::string(item["ID"].GetString())))
+                    std::string server;
+                    auto findPos = product.productIdentifier.rfind(".");
+                    if (findPos != std::string::npos)
+                    {
+                        server = product.productIdentifier.substr(0, findPos + 1);
+                    }
+
+                    const std::string &productIdentifier = server + std::string(item["ID"].GetString());
+
+                    if (product.productIdentifier == productIdentifier)
                     {
                         auto &localKey = item["LocaleKey"];
                         product.localizedName = std::string(localKey[_currentLocale.c_str()].GetString());
