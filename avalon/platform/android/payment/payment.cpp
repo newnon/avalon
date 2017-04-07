faeaca94816d75b17e30ae9aae192b3d8e42793f
Sergey <non_2001@mail.ru>
2017-04-07 15:44:20 +0300

    fix android payments

--- avalon/platform/android/payment/payment.cpp
+++ avalon/platform/android/payment/payment.cpp
@@ -224,16 +224,15 @@ JNIEXPORT void JNICALL Java_com_avalon_payment_Backend_delegateOnPurchaseSucceed
         return;
     }
 
-    std::string productId = cocos2d::JniHelper::jstring2string(jProductId);
-    std::string token = cocos2d::JniHelper::jstring2string(jToken);
-    std::string orderId = cocos2d::JniHelper::jstring2string(jOrderId);
-    const Product* product = globalManager->getProductByProductIdentifier(productId.c_str());
-
     if (globalManager->getDelegate()) {
+        std::string productId = cocos2d::JniHelper::jstring2string(jProductId);
+        std::string token = cocos2d::JniHelper::jstring2string(jToken);
+        std::string orderId = cocos2d::JniHelper::jstring2string(jOrderId);
+        const Product* product = globalManager->getProductByProductIdentifier(productId.c_str());
     	Transaction managerTransaction;
     	if(product)
     	{
-    		managerTransaction.productId = product->productIdentifier;
+    		managerTransaction.productId = product->id;
             managerTransaction.transactionIdentifier = orderId;
             managerTransaction.receipt = std::vector<unsigned char>(token.begin(), token.end());
             managerTransaction.transactionState = jRestored?TransactionState::Purchased:TransactionState::Restored;
@@ -252,12 +251,18 @@ JNIEXPORT void JNICALL Java_com_avalon_payment_Backend_delegateOnPurchaseSucceed
 
 JNIEXPORT void JNICALL Java_com_avalon_payment_Backend_delegateOnPurchaseFail(JNIEnv* env, jclass clazz, jstring jProductId, jint code)
 {
-	Manager *globalManager = Manager::getInstance();
+    AndroidManager *globalManager = static_cast<AndroidManager*>(Manager::getInstance());
     AVALON_ASSERT_MSG(globalManager, "globalManager should be already set");
 
-    if (globalManager && globalManager->getDelegate()) {
+    if (!globalManager) {
+        return;
+    }
+
+    if (globalManager->getDelegate()) {
+        std::string productId = cocos2d::JniHelper::jstring2string(jProductId);
+        const Product* product = globalManager->getProductByProductIdentifier(productId.c_str());
     	Transaction managerTransaction;
-    	managerTransaction.productId = cocos2d::JniHelper::jstring2string(jProductId);
+        managerTransaction.productId = product?product->id:productId;
     	managerTransaction.transactionState = TransactionState::Failed;
     	globalManager->getDelegate()->onPurchaseFail(managerTransaction, static_cast<avalon::payment::ManagerDelegateErrors>(code));
     }
