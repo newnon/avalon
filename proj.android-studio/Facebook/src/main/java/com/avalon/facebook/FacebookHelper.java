90ec05ac93e42cb9310704c1190b1f9bcaaed3af
Vitaliy <xvacid@gmail.com>
2017-04-25 19:08:31 +0300

    bug fix fb for android

--- proj.android-studio/Facebook/src/main/java/com/avalon/facebook/FacebookHelper.java
+++ proj.android-studio/Facebook/src/main/java/com/avalon/facebook/FacebookHelper.java
@@ -191,7 +191,8 @@ public class FacebookHelper
 
     public static boolean isLoggedIn()
     {
-        if (!AccessToken.getCurrentAccessToken().getToken().isEmpty())
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        if (token != null && !token.getToken().isEmpty())
             return true;
         return false;
     }
